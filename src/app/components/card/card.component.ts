353d920e0f6913b490504f039671b0e2d3801778
enesozmert <en-es111@hotmail.com>
2021-03-24 18:39:14 +0300

    24.03.2021 homework 3

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,5 +1,8 @@
+import { CarImageDetailDtoService } from './../../services/car-image-detail-dto.service';
 import { CarDetailDtoService } from './../../services/car-detail-dto.service';
 import { Component, OnInit } from '@angular/core';
+import { CarImageDetailDto } from 'src/app/models/dtos/carImageDetailDto';
+import { Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -7,10 +10,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent implements OnInit {
-
-  constructor(private carDetailDtoService:CarDetailDtoService) { }
+  carImageDetailDtos: CarImageDetailDto[] = []
+  dataLoaded: boolean = false
+  
+  @Input() carId:number
+  constructor(private carDetailDtoService:CarDetailDtoService,
+    private carImageDetailDtoService:CarImageDetailDtoService) { }
 
   ngOnInit(): void {
+    
   }
-
+  
 }
